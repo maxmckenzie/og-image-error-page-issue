9162eb7bd4ca904eacfaf3a5dc4f67724c6ddf35
Max McKenzie <hello@maxs.studio>
2025-07-15 11:53:23 +0200

    reproduction

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineOgImageComponent("Default", {
+  title: "My page title",
+  description: "description stuff",
+  imagePath: "/images/pup.jpg",
+});
+</script>
+
+<template>
+  <NuxtPage />
+</template>
---
+++ components/OgImage/Default.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const { title, description, imagePath } = defineProps<{
+  title?: string;
+  description?: string;
+  imagePath: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <img :src="imagePath" alt="Image" />
+    <div class="main">
+      <h1>
+        {{ title }}
+      </h1>
+      <p>
+        {{ description }}
+      </p>
+  </div>
+</template>
